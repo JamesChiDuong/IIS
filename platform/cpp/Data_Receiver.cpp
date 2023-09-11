de7d46b3b7e740f21730fee01d69feb4c4e6847b
Ruben <ruben@polycephaly.org>
2023-09-11 17:35:32 +0800

    simplify

--- platform/cpp/Data_Receiver.cpp
+++ platform/cpp/Data_Receiver.cpp
@@ -23,107 +23,26 @@ int	main(int argc, char **argv)
 {
    Verilated::commandArgs(argc, argv);
    UARTSIM		*uart;										// init uart pointer
-   uart_PseudoTerminal Pseudo;							// Init Pseudo terminal
    unsigned	setup = 868;								// init baudrate
-   int fd;
 
-   int	childs_stdin[2], childs_stdout[2];
-   if ((pipe(childs_stdin)!=0)||(pipe(childs_stdout) != 0)) 
-   {
-      fprintf(stderr, "ERR setting up child pipes\n");
-      perror("O/S ERR");
-      printf("TEST FAILURE\n");
-      exit(EXIT_FAILURE);
-   }
+   // Setup the model and baud rate
+   SIMCLASS tb;
+   int baudclocks = setup & 0x0ffffff;
+   tb.i_uart_rx = 1;
 
-   // Set up first child process
-   pid_t childs_pid = fork();
+   uart = new UARTSIM();
+   uart->setup(setup);
 
-   if (childs_pid != 0) 
-   { // The parent, feeding the simulation
+   // Make sure we don't run longer than 4 seconds ...
+   unsigned	clocks = 0;
 
-      while (1) // re-open pseudo terminal if closed by client
-      {
-         // We are the parent
-         //close(childs_stdin[ 0]); // Close the read end
-         //close(childs_stdout[1]); // Close the write end
-
-         Pseudo.PseudoTerminal_Init(&fd);						// Init Pesudo Terminal
-
-         pid_t childs_pid = fork();
-         if (childs_pid != 0) 
-         { // the parent - forward pseudo terminal to simulation
-            char Buffer; //[100];
-
-            while (1)
-            {
-               if (Pseudo.PseudoTerminal_readByte(fd, &Buffer) < 0)
-               {
-                  fprintf(stderr, "re-open pts\n");
-                  break;
-               }
-
-               write(childs_stdin[1], &Buffer, 1);
-            }
-
-            kill(childs_pid, SIGTERM);
-            int stat;
-            wait(&stat);
-
-            Pseudo.PseudoTerminal_Deinit(fd);
-         }
-         else
-         { // the child
-            while(1)
-            {
-               char a;
-               read(childs_stdout[0],&a, 1);
-
-               Pseudo.PseudoTerminal_writeByte(fd,a);
-            }
-         }
-      }
-   }
-   else 
-   { // The child (Verilator simulation)
-
-      // Fix up the FILE I/O
-      close(childs_stdin[ 1]);
-      close(childs_stdout[0]);
-      close(STDIN_FILENO);
-      if (dup(childs_stdin[0]) < 0) 
-      {
-         fprintf(stderr, "ERR setting up child FD\n");
-         perror("O/S ERR");
-         exit(EXIT_FAILURE);
-      }
-      close(STDOUT_FILENO); 
-      if (dup(childs_stdout[1]) < 0) 
-      {
-         fprintf(stderr, "ERR setting up child FD\n");
-         perror("O/S ERR");
-         exit(EXIT_FAILURE);
-      }
-
-      // Setup the model and baud rate
-      SIMCLASS tb;
-      int baudclocks = setup & 0x0ffffff;
-      tb.i_uart_rx = 1;
-
-      // UARTSIM(0) uses stdin and stdout for its FD's
-      uart = new UARTSIM(0);
-      uart->setup(setup);
-
-      // Make sure we don't run longer than 4 seconds ...
-      unsigned	clocks = 0;
-
-      // VCD trace setup
+   // VCD trace setup
 #define	VCDTRACE
 #ifdef	VCDTRACE
-      Verilated::traceEverOn(true);
-      VerilatedVcdC* tfp = new VerilatedVcdC;
-      tb.trace(tfp, 99);
-      tfp->open(WAVENAME);
+   Verilated::traceEverOn(true);
+   VerilatedVcdC* tfp = new VerilatedVcdC;
+   tb.trace(tfp, 99);
+   tfp->open(WAVENAME);
 #define	TRACE_POSEDGE	tfp->dump(10*clocks)
 #define	TRACE_NEGEDGE	tfp->dump(10*clocks+5)
 #define	TRACE_CLOSE	tfp->close()
@@ -133,31 +52,30 @@ int	main(int argc, char **argv)
 #define	TRACE_CLOSE	while(0)
 #endif
 
-      // Clear any initial break condition
-      for(int i=0; i<(baudclocks*24); i++) {
-         tb.clk = 1;
-         tb.eval();
-         tb.clk = 0;
-         tb.eval();
+   // Clear any initial break condition
+   for(int i=0; i<(baudclocks*24); i++) {
+      tb.clk = 1;
+      tb.eval();
+      tb.clk = 0;
+      tb.eval();
 
-         tb.i_uart_rx = 1;
-      }
-
-      // Simulation loop: process the hello world string
-      while(1)
-      {
-         tb.clk = 1;
-         tb.eval();
-         TRACE_POSEDGE;
-         tb.clk = 0;
-         tb.eval();
-         TRACE_NEGEDGE;
-         clocks++;
-         tb.i_uart_rx = (*uart)(tb.o_uart_tx);
-      }
-      TRACE_CLOSE;
-
-      exit(EXIT_SUCCESS);
+      tb.i_uart_rx = 1;
    }
+
+   // Simulation loop: process the hello world string
+   while(1)
+   {
+      tb.clk = 1;
+      tb.eval();
+      TRACE_POSEDGE;
+      tb.clk = 0;
+      tb.eval();
+      TRACE_NEGEDGE;
+      clocks++;
+      tb.i_uart_rx = (*uart)(tb.o_uart_tx);
+   }
+   TRACE_CLOSE;
+
+   exit(EXIT_SUCCESS);
 }
 
